0e9464b41d0a01a604a79b38ce36a7d4d053a3f5
Molefe Molefe <1858893@students.wits.ac.za>
2025-10-01 14:10:05 +0200

    Chest X Ray

--- main.py
+++ main.py
@@ -338,10 +338,8 @@ class ImageLogger(Callback):
     @rank_zero_only
     def log_local(self, save_dir, split, images,
                   global_step, current_epoch, batch_idx):
-        if save_dir is None:
-            print(f"Warning: logger save_dir is None. Skipping local image saving for step {global_step}.")
-            return
         root = os.path.join(save_dir, "images", split)
+        os.makedirs(root, exist_ok=True)
         for k in images:
             grid = torchvision.utils.make_grid(images[k], nrow=4)
             if self.rescale:
